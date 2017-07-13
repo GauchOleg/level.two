5fdaaa5597419aa235f3493b5dbeba74d569b290
Oleg Gauch <gauch_oleg@ukr.net>
2017-07-13 23:32:58 +0300

    small change

--- test/q3.php
+++ test/q3.php
@@ -3,9 +3,9 @@
 		<td align="left">
 		<p>Сколько будет 5*5?</p>
 		<form action='<?php echo $_SERVER['REQUEST_URI']?>' method='post'>
-			<input type='radio' name='answer' value='c1'>25<br>
-			<input type='radio' name='answer' value='c2'>20<br>
-			<input type='radio' name='answer' value='c3'>15<br>
+			<input type='radio' name='answer' value='<?= ++$q?>'>25<br>
+			<input type='radio' name='answer' value='<?= ++$q?>'>20<br>
+			<input type='radio' name='answer' value='<?= ++$q?>'>15<br>
 			<input type='hidden' name='title' value='Получите результат'>
 			<input type='submit' value='Ответить'>
 		</form>
--- test/start.php
+++ test/start.php
@@ -2,7 +2,7 @@
 	<tr>
 		<td align="left">
 		<form action='<?php echo $_SERVER['REQUEST_URI']?>' method='post'>
-			<input type='hidden' name='title' value='Ответьте на вопрос'>
+			<input type='hidden' name='title' value='<?= ++$q?>'>
 			<input type='submit' value='Начать тест'>
 		</form>
 		</td>
